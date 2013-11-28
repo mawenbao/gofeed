c81e9c47199ae4338704b43649f6ca9c45746239
wilbur ma <mawenbao@hotmail.com>
2013-11-28 12:45:38 +0800

    format code with gofmt

--- gofeed.go
+++ gofeed.go
@@ -18,7 +18,7 @@ var (
 	gVerbose = flag.Bool("v", false, "be verbose")
 	gDebug   = flag.Bool("d", false, "debug mode")
 	gCPUNum  = flag.Int("c", runtime.NumCPU(), "number of cpus to run simultaneously")
-    gLogfile = flag.String("l", "", "path of the logfile")
+	gLogfile = flag.String("l", "", "path of the log file")
 )
 
 func showUsage() {
@@ -43,17 +43,17 @@ func main() {
 		*gCPUNum = runtime.NumCPU()
 	}
 
-    var logfile *os.File
+	var logfile *os.File
 	var err error
 
-    if "" != *gLogfile {
-        logfile, err = os.OpenFile(*gLogfile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-        if nil != err {
-            log.Fatalf("failed to open/create logfile %s: %s", *gLogfile, err)
-        }
-    }
-    defer logfile.Close()
-    log.SetOutput(logfile)
+	if "" != *gLogfile {
+		logfile, err = os.OpenFile(*gLogfile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if nil != err {
+			log.Fatalf("failed to open/create logfile %s: %s", *gLogfile, err)
+		}
+	}
+	defer logfile.Close()
+	log.SetOutput(logfile)
 
 	// debug mode is verbose
 	if *gDebug {
@@ -82,7 +82,7 @@ func main() {
 
 		go func(feedTar *FeedTarget) {
 			defer wg.Done()
-            log.Printf("start processing target %s", feedTar.FeedPath)
+			log.Printf("start processing target %s", feedTar.FeedPath)
 			// parse feed entry title and link
 			feed, ok := ParseIndexHtml(feedTar)
 			if !ok {
@@ -103,7 +103,7 @@ func main() {
 				if nil != err {
 					log.Printf("[ERROR] failed to save feed at %s", feedTar.FeedPath)
 				}
-                log.Printf("[DONE] saving feed at %s", feedTar.FeedPath)
+				log.Printf("[DONE] saving feed at %s", feedTar.FeedPath)
 			}
 		}(feedTar)
 	}
